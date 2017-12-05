6f29b9ec9b74dd16bafee63e716b39b770802b37
Devitgg <txux.7jj@gmail.com>
2017-12-05 18:21:05 -0500

    Quick Fix
    whoops, made phone_number a string instead of an int.

--- database/migrations/2017_12_04_221653_ContactEmails.php
+++ database/migrations/2017_12_04_221653_ContactEmails.php
@@ -17,7 +17,7 @@ class ContactEmails extends Migration
              $table->increments('id');
              $table->string('name');
              $table->string('email');
-             $table->string('phone_number')->nullable();
+             $table->integer('phone_number')->nullable();
              $table->longText('content');
              $table->timestamps();
          });
